5582b4caae6b3ebcb73e57758274ad55ec486c59
Prajwal Negi <166615651+prajwal2301negi@users.noreply.github.com>
2025-06-12 03:42:39 +0530

    Update middleware.ts

--- middleware.ts
+++ middleware.ts
@@ -1,23 +1,35 @@
 //import { clerkMiddleware } from '@clerk/nextjs/server';
-import { clerkMiddleware } from "@clerk/nextjs/edge";
+// import { clerkMiddleware } from "@clerk/nextjs/edge";
 
-export default clerkMiddleware();
+// export default clerkMiddleware();
 
-export const config = {
-  // matcher: [
-  //   // Skip Next.js internals and all static files, unless found in search params
-  //   '/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)',
-  //   // Always run for API routes
-  //   '/(api|trpc)(.*)',
-  // ],
+// export const config = {
+//   // matcher: [
+//   //   // Skip Next.js internals and all static files, unless found in search params
+//   //   '/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)',
+//   //   // Always run for API routes
+//   //   '/(api|trpc)(.*)',
+//   // ],
 
-  // matcher: ['/((?!_next/image|_next/static|favicon.ico).*)'],
-   matcher: [
-    // Apply Clerk middleware to everything in /app/*
-    '/((?!.*\\..*|_next).*)', // excludes static files
-    '/',                      // root route
-    '/(api|trpc)(.*)',        // API routes
-    '/app/(.*)'               // 💥 This line ensures app actions get the auth context
-  ],
+//   // matcher: ['/((?!_next/image|_next/static|favicon.ico).*)'],
+//    matcher: [
+//     // Apply Clerk middleware to everything in /app/*
+//     '/((?!.*\\..*|_next).*)', // excludes static files
+//     '/',                      // root route
+//     '/(api|trpc)(.*)',        // API routes
+//     '/app/(.*)'               // 💥 This line ensures app actions get the auth context
+//   ],
 
 };
+import { clerkMiddleware } from '@clerk/nextjs/server'
+
+export default clerkMiddleware()
+
+export const config = {
+  matcher: [
+    // Skip Next.js internals and all static files, unless found in search params
+    '/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)',
+    // Always run for API routes
+    '/(api|trpc)(.*)',
+  ],
+}
